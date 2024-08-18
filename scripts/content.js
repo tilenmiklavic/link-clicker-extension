2c0f0fa87108f6d0fcd76ca683824e551df64777
Tilen Miklavic <tilen.miklavic@gmail.com>
2024-08-18 12:14:33 +0200

    feat: open link on click

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,20 @@
 chrome.runtime.onMessage.addListener((request) => {
-  console.log("Message from the background script:");
+  const LINK_CLASS_NAME = "UWckNb"
   console.log(request);
   
   // get list of all links on page
   var links = [...document.getElementsByTagName("a")];
-  // console.log(links);
 
-  // for (var i = 0; i < links.length; i++) {
-  //   links[i].jsname = links[i].getAttribute("jsname")
-  //   // console.log(i, links[i].getAttribute("jsname"));
-  // }
+  const valid_links = links.filter((link) => link.getAttribute("jsname") === LINK_CLASS_NAME);
 
-  console.log(links);
-  const valid_links = links.filter((link) => link.getAttribute("jsname") === "UWckNb");
-
-  console.log(valid_links);
+  document.addEventListener("keypress", function(event) {
+    link_press(event, valid_links)
+  });
 
   return Promise.resolve({ response: "Hi from content script" });
 });
+
+function link_press (e, links) {
+  let ind = e.keyCode - 49;
+  links[ind].click();
+}
